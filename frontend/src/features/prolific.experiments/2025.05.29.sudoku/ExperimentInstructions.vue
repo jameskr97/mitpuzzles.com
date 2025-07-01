d409277572e145f8b1086badec5702ca0e2431a0
James Roberts <james@jameskr.dev>
2025-07-01 14:48:17 -0400

    refactor: update looks of instruction

--- frontend/src/features/prolific.experiments/2025.05.29.sudoku/ExperimentInstructions.vue
+++ frontend/src/features/prolific.experiments/2025.05.29.sudoku/ExperimentInstructions.vue
@@ -11,6 +11,8 @@ import { withSudokuBehaviors } from "@/features/games/sudoku/useSudokuCellHighli
 import { withSudokuFocusBehavior } from "@/features/games/sudoku/useSudokuFocusHighlighter.ts";
 import { usePuzzleController } from "@/composables/usePuzzleController.ts";
 import { useRoute } from "vue-router";
+import { shuffle } from "@/utils.ts";
+import Container from "@/components/ui/Container.vue";
 
 const props = defineProps({
   context: {
@@ -117,18 +119,14 @@ const BoxHighlighted = getPersistentHighlightSudokuBoard({
 });
 
 const showQuizWarning = ref(false);
-let quiz: { answer: boolean; question: string }[] =
-  /*shuffle(*/
-  [
-    { answer: true, question: "Each row must have the numbers 1 to 9, with no repeats." },
-    { answer: true, question: "Each column must include every number from 1 to 9 exactly once." },
-    { answer: true, question: "Each 3x3 box must include every number from 1 to 9 exactly once." },
-    { answer: false, question: "You can use the same number multiple times in a column." },
-    { answer: false, question: "A 3x3 box can include the same number multiple times." },
-    { answer: false, question: "The starting numbers can be changed." },
-  ];
-
-//);
+let quiz: { answer: boolean; question: string }[] = shuffle([
+  { answer: true, question: "Each row must have the numbers 1 to 9, with no repeats." },
+  { answer: true, question: "Each column must include every number from 1 to 9 exactly once." },
+  { answer: true, question: "Each 3x3 box must include every number from 1 to 9 exactly once." },
+  { answer: false, question: "You can use the same number multiple times in a column." },
+  { answer: false, question: "A 3x3 box can include the same number multiple times." },
+  { answer: false, question: "The starting numbers can be changed." },
+]);
 
 function onQuizSubmitted(allCorrect: boolean) {
   if (allCorrect) {
@@ -140,79 +138,86 @@ function onQuizSubmitted(allCorrect: boolean) {
 </script>
 
 <template>
-  <div class="container contents-main-wrapper mx-auto max-w-[75ch] flex flex-col prose">
-        <h3 class="text-2xl font-semibold text-slate-700 border-b-2 border-b-primary my-2">Game Overview</h3>
+  <div class="container contents-main-wrapper mx-auto max-w-[78ch] flex flex-col prose">
+    <h3 class="text-2xl font-semibold text-slate-700 border-b-2 border-b-primary my-2">Game Overview</h3>
 
-        <div>
-          <p>
-            Sudoku is a logic puzzle played on a square. The grid is divided into 9 rows and 9 columns, forming 9 smaller
-            grids (3x3 squares). In order to solve a sudoku board, every square must be filled in with a number from 1 to 9.
-            Some numbers are already filled in when you start, and they cannot be changed.
-          </p>
-          <p>
-            Your job is figuring out which numbers go into the empty squares by looking at numbers that are already filled
-            in the row, column, or 3x3 squares.
-          </p>
-          <p>However, there are some important rules around how many times you can use each number!</p>
+    <div>
+      <p>
+        Sudoku is a logic puzzle played on a square. The grid is divided into 9 rows and 9 columns, forming 9 smaller
+        grids (3x3 squares). In order to solve a sudoku board, every square must be filled in with a number from 1 to 9.
+        Some numbers are already filled in when you start, and they cannot be changed.
+      </p>
+      <p>
+        Your job is figuring out which numbers go into the empty squares by looking at numbers that are already filled
+        in the row, column, or 3x3 squares.
+      </p>
+      <p>However, there are some important rules around how many times you can use each number!</p>
+    </div>
+
+    <h3 class="text-2xl font-semibold text-slate-700 border-b-2 border-b-primary my-2">Sudoku Rules</h3>
+
+    <div class="grid grid-cols-[auto_min-content] gap-x-5 gap-y-10 w-full">
+      <div class="grid col-span-2 grid-cols-subgrid">
+        <div class="flex flex-col gap-1">
+          <div>
+            First, each <strong>row</strong> must include every number from 1 to 9, and each number must appear exactly
+            <strong>once</strong>.
+          </div>
+          <div class="italic">
+            The highlighted row has the numbers 3, 4, 2, 6, 5, and 1. It needs the numbers 7, 8, and 9 for the row to be
+            complete.
+          </div>
+          <Container class="border-black/10 bg-amber-500/20! border-1">
+            <div>
+              For example, 8 could be in any one of the locations
+              <span class="font-bold">highlighted with an orange border</span>.
+            </div>
+          </Container>
         </div>
+        <RowHighlighted :state="gameState0" :scale="0.75"></RowHighlighted>
+      </div>
 
-        <h3 class="text-2xl font-semibold text-slate-700 border-b-2 border-b-primary my-2">Sudoku Rules</h3>
-
-        <div class="grid grid-cols-2 gap-x-5 gap-y-10 w-full">
-          <div class="flex flex-col gap-1 row-start-1">
-            <div>
-              First, each <strong>row</strong> must include every number from 1 to 9, and each number must appear exactly
-              <strong>once</strong>.
-            </div>
-            <div class="italic">
-              The highlighted row has the numbers 3, 4, 2, 6, 5, and 1. It needs the numbers 7, 8, and 9 for the row to be
-              complete.
-            </div>
-            <Container class="border-black/10 bg-amber-500/20! border-1">
-              <div>
-                For example, 8 could be in any one of the locations
-                <span class="font-bold">highlighted with an orange border</span>.
-              </div>
-            </Container>
+      <div class="grid col-span-2 grid-cols-subgrid">
+        <div class="flex flex-col gap-4">
+          <div>
+            Second, each <strong>column</strong> must include every number from 1 to 9, and each number must appear
+            exactly <strong>once</strong>.
           </div>
-          <RowHighlighted :state="gameState0" :scale="0.75"></RowHighlighted>
-
-          <div class="flex flex-col gap-4 row-start-2">
-            <div>
-              Second, each <strong>column</strong> must include every number from 1 to 9, and each number must appear
-              exactly <strong>once</strong>.
-            </div>
-            <div class="italic">
-              The highlighted row has the numbers 6, 8, 3, 4, and 1. It needs the numbers 2, 5, 7, and 9 for the column to
-              be complete.
-            </div>
-            <Container class="border-black/10 bg-amber-500/20! border-1">
-              <div>
-                Since the highlighted column already contains an 8, this column is <span class="font-bold">ruled out</span>
-                as a possible location for 8 in the third row.
-              </div>
-            </Container>
+          <div class="italic">
+            The highlighted row has the numbers 6, 8, 3, 4, and 1. It needs the numbers 2, 5, 7, and 9 for the column to
+            be complete.
           </div>
-          <ColHighlighted :state="gameState0" :scale="0.75"></ColHighlighted>
-
-          <div class="flex flex-col gap-4 row-start-3">
+          <Container class="border-black/10 bg-amber-500/20! border-1">
             <div>
-              Third, each <strong>box</strong> must include every number from 1 to 9, and each number must appear exactly
-              <strong>once</strong>.
+              Since the highlighted column already contains an 8, this column is
+              <span class="font-bold">ruled out</span>
+              as a possible location for 8 in the third row.
             </div>
-            <div class="italic">
-              The highlighted box has the numbers 9, 2, 4, 8, 3, and 1. It needs the numbers 5, 6, and 7 for the box to be
-              complete.
-            </div>
-            <Container class="border-black/10 bg-amber-500/20! border-1">
-              <div>
-                Since the highlighted box already contains an 8, it rules out one more location for 8 in the third row. This
-                allows us <span class="font-bold">to deduce the correct location for 8 in the third row</span>.
-              </div>
-            </Container>
-          </div>
-          <BoxHighlighted :state="gameState0With8" :scale="0.75"></BoxHighlighted>
+          </Container>
         </div>
+        <ColHighlighted :state="gameState0" :scale="0.75"></ColHighlighted>
+      </div>
+
+      <div class="grid col-span-2 grid-cols-subgrid">
+        <div class="flex flex-col gap-4">
+          <div>
+            Third, each <strong>box</strong> must include every number from 1 to 9, and each number must appear exactly
+            <strong>once</strong>.
+          </div>
+          <div class="italic">
+            The highlighted box has the numbers 9, 2, 4, 8, 3, and 1. It needs the numbers 5, 6, and 7 for the box to be
+            complete.
+          </div>
+          <Container class="border-black/10 bg-amber-500/20! border-1">
+            <div>
+              Since the highlighted box already contains an 8, it rules out one more location for 8 in the third row.
+              This allows us <span class="font-bold">to deduce the correct location for 8 in the third row</span>.
+            </div>
+          </Container>
+        </div>
+        <BoxHighlighted :state="gameState0With8" :scale="0.75"></BoxHighlighted>
+      </div>
+    </div>
 
     <h3 class="text-2xl font-semibold text-slate-700 border-b-2 border-b-primary pb-2 mb-4">Additional Challenge</h3>
     <p>
@@ -231,11 +236,14 @@ function onQuizSubmitted(allCorrect: boolean) {
       <li>
         <strong>Reveal:</strong> Click on a cell to reveal that cell
         <div class="inline-block h-5 w-5 bg-slate-300 rounded border-1 border-black/40"></div>
-        (dark blue) and the row, column, and box which it belongs to (light blue) <div class="inline-block h-5 w-5 bg-slate-200 rounded border-1 border-black/40"></div>.
+        (dark blue) and the row, column, and box which it belongs to (light blue)
+        <div class="inline-block h-5 w-5 bg-slate-200 rounded border-1 border-black/40"></div>
+        .
       </li>
       <li>
         <strong>Input:</strong> Enter a number in the selected cell (dark blue) by pressing number keys 1 to 9 on your
-        keyboard. Numbers you entered will appear in <span class="text-sky-600 font-bold">blue</span>, and can be changed again later.
+        keyboard. Numbers you entered will appear in <span class="text-sky-600 font-bold">blue</span>, and can be
+        changed again later.
       </li>
     </ul>
     <div class="flex flex-col w-full items-center">
